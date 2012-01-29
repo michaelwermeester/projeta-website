10288a90d94d3e7b40bd9e75b69a205dbd012383
Michael Wermeester <michael.wermeester@me.com>
2012-01-29 21:05:58 +0100

    Add simple login.

---
+++ src/java/projetaBeans/AuthBackingBean.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetaBeans;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author michael
+ */
+@ManagedBean
+@RequestScoped
+public class AuthBackingBean {
+
+    /** Creates a new instance of AuthBackingBean */
+    public AuthBackingBean() {
+    }
+    
+    private static final Logger log = Logger.getLogger(AuthBackingBean.class.getName());
+
+    public String logout() {
+        String result = "/index?faces-redirect=true";
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            log.log(Level.SEVERE, "Failed to logout user!", e);
+            result = "/loginError?faces-redirect=true";
+        }
+
+        return result;
+    }
+}
