65661b5d4630e30cae18bed4c66b843a255db213
Michael Wermeester <michael.wermeester@me.com>
2012-02-14 00:17:17 +0100

    Commentaires.

--- src/java/projetaBeans/AuthBackingBean.java
+++ src/java/projetaBeans/AuthBackingBean.java
@@ -23,16 +23,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequestScoped
 public class AuthBackingBean {
 
-    /** Creates a new instance of AuthBackingBean */
-    
-    private String username;
+    // nom d'utilisateur.
+    private String username; 
+    // mot de passe.
     private String password;
     
+    // Constructeur.
     public AuthBackingBean() {
     }
     
     private static final Logger log = Logger.getLogger(AuthBackingBean.class.getName());
 
+    // Déconnecter l'utilisateur.
     public String logout() {
         String result = "/index.xhtml?faces-redirect=true";
 
@@ -49,6 +51,8 @@ public class AuthBackingBean {
         return result;
     }
     
+    // Propriété : retourne true si l'utilisateur a une session ouverte.
+    //             retourne false si l'utilisateur n'est pas connecté.
     public Boolean getLoggedIn() {
         
         FacesContext context = FacesContext.getCurrentInstance();
@@ -60,23 +64,27 @@ public class AuthBackingBean {
             return true;
     } 
     
-   
+    // getter pour username (nom d'utilisateur).
     public String getUsername(){
         return username;
     }
     
+    // setter pour username (nom d'utilisateur).
     public void setUsername(String username) {
         this.username = username;
     }
     
+    // getter pour password (mot de passe).
     public String getPassword(){
         return password;
     }
     
+    // setter pour password (mot de passe).
     public void setPassword(String password) {
         this.password = password;
     }
     
+    // ouvrir une session en utilisant le username spécifié.
     public String login() {
         
         FacesContext context = FacesContext.getCurrentInstance();
