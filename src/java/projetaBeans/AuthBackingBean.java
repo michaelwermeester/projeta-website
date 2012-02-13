6ac1e52282e04e8c24e8d115f56736c0f9d77f5f
Michael Wermeester <michael.wermeester@me.com>
2012-02-14 00:02:37 +0100

    - New login (draft) - progress.

--- src/java/projetaBeans/AuthBackingBean.java
+++ src/java/projetaBeans/AuthBackingBean.java
@@ -6,8 +6,11 @@ package projetaBeans;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +24,10 @@ import javax.servlet.http.HttpServletRequest;
 public class AuthBackingBean {
 
     /** Creates a new instance of AuthBackingBean */
+    
+    private String username;
+    private String password;
+    
     public AuthBackingBean() {
     }
     
@@ -42,5 +49,50 @@ public class AuthBackingBean {
         return result;
     }
     
+    public Boolean getLoggedIn() {
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        
+        if (request.getUserPrincipal() == null)
+            return false;
+        else 
+            return true;
+    } 
+    
    
+    public String getUsername(){
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String login() {
+        
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        
+        try {
+            request.login(username, password);
+            
+            return "regConfirmation.xhtml?faces-redirect=true";
+        } catch (Exception ex) {
+            
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login incorrect", "Login incorrect");
+
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            
+            return "loginForm.xhtml";
+        }
+    }
 }
