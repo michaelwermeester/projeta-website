e402898d30a34bef565119a396e2039635e1a125
Michael Wermeester <michael.wermeester@me.com>
2012-04-21 13:57:06 +0200

    Show comments for a task.

---
+++ src/java/projetaBeans/CommentCocoaController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetaBeans;
+
+import be.luckycode.projetawebservice.Comment;
+import be.luckycode.projetawebservice.CommentDummy;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import pojeta.Common;
+import pojeta.WSCommentHelper;
+
+/**
+ *
+ * @author michael
+ */
+@ManagedBean
+@RequestScoped
+public class CommentCocoaController {
+
+    private List<Comment> commentList;
+    
+    private WSCommentHelper wch;
+    
+    // ID de du projet, tâche, bug.
+    private String requestId;
+    // type (project, task, bug).
+    private String requestType;
+    
+    /**
+     * Creates a new instance of CommentCocoaController
+     */
+    public CommentCocoaController() {
+        
+        HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        // lire ID de du projet, tâche, bug.
+        this.requestId = request.getParameter("id");
+        // lire le type (projet, tâche, bug).
+        this.requestType = request.getParameter("type");
+    }
+
+    /**
+     * @return the commentList
+     */
+    public List<Comment> getCommentList() {
+        
+        
+        wch = new WSCommentHelper();
+        wch.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+        
+        // si le type = "task".
+        if (requestType.equals("task")) {
+            CommentDummy tmpCommentDummy = wch.findCommentsByTaskIdWebsite(CommentDummy.class, this.requestId);
+        
+            this.commentList = tmpCommentDummy.getListComment();
+        }
+        
+        return commentList;
+    }
+
+    /**
+     * @param commentList the commentList to set
+     */
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+}
