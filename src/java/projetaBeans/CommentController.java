b72540696147d39da025b458c34332b9db1c3340
Michael Wermeester <michael.wermeester@me.com>
2012-04-29 22:24:46 +0200

    Bug fixes.

--- src/java/projetaBeans/CommentController.java
+++ src/java/projetaBeans/CommentController.java
@@ -30,17 +30,13 @@ public class CommentController {
      * Creates a new instance of CommentController
      */
     private List<Comment> commentList;
-    
     private WSCommentHelper commentHelper;
-    
     private String comment;
     private Comment commentObject;
-    
     // bug = b; task = t; project = p.
-    private char commentType; 
-    
+    private char commentType;
     private ProjectSimple project;
-    
+
     public CommentController() {
     }
 
@@ -48,15 +44,19 @@ public class CommentController {
      * @return the commentList
      */
     public List<Comment> getCommentList() {
-        
-        
-        /*wch = new WSCommentHelper();
-        wch.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
-        
-        CommentDummy tmpCommentDummy = wch.findCommentsByTaskIdWebsite(CommentDummy.class, "2");
-        
-        this.commentList = tmpCommentDummy.getListComment();*/
-        
+
+
+        /*
+         * wch = new WSCommentHelper();
+         * wch.setUsernamePassword(Common.getWSUsername(),
+         * Common.getWSPassword());
+         *
+         * CommentDummy tmpCommentDummy =
+         * wch.findCommentsByTaskIdWebsite(CommentDummy.class, "2");
+         *
+         * this.commentList = tmpCommentDummy.getListComment();
+         */
+
         return commentList;
     }
 
@@ -94,70 +94,74 @@ public class CommentController {
     public void setCommentType(char commentType) {
         this.commentType = commentType;
     }
-    
+
     public String showProjectComments() {
-        
+
         //this.projectId = Integer.parseInt(getStrProjectId());
-        
+
         //this.projectId = projectSimple.getId();
-        
-        commentHelper = new WSCommentHelper();
-        commentHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
-        
-        CommentDummy tmpCommentDummy = commentHelper.findCommentsByProjectIdWebsite(CommentDummy.class, project.getId().toString());
-        
-        this.commentList = tmpCommentDummy.getListComment();
-        
-        // line breaks.
-        for (Comment c : commentList) {
-            c.setComment(c.getComment().replace("\n", "<br />"));
+        try {
+            commentHelper = new WSCommentHelper();
+            commentHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+
+            CommentDummy tmpCommentDummy = commentHelper.findCommentsByProjectIdWebsite(CommentDummy.class, project.getId().toString());
+
+            this.commentList = tmpCommentDummy.getListComment();
+
+            // line breaks.
+            for (Comment c : commentList) {
+                c.setComment(c.getComment().replace("\n", "<br />"));
+            }
+        } catch (Exception e) {
         }
-        
+
         return "comments.xhtml?faces-redirect=true";
     }
-    
+
     public String showTaskComments() {
-        
+
         //this.projectId = Integer.parseInt(getStrProjectId());
-        
+
         //this.projectId = projectSimple.getId();
-        
-        commentHelper = new WSCommentHelper();
-        commentHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
-        
-        CommentDummy tmpCommentDummy = commentHelper.findCommentsByTaskIdWebsite(CommentDummy.class, project.getId().toString());
-        
-        this.commentList = tmpCommentDummy.getListComment();
-        
-        // line breaks.
-        for (Comment c : commentList) {
-            c.setComment(c.getComment().replace("\n", "<br />"));
+        try {
+            commentHelper = new WSCommentHelper();
+            commentHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+
+            CommentDummy tmpCommentDummy = commentHelper.findCommentsByTaskIdWebsite(CommentDummy.class, project.getId().toString());
+
+            this.commentList = tmpCommentDummy.getListComment();
+
+            // line breaks.
+            for (Comment c : commentList) {
+                c.setComment(c.getComment().replace("\n", "<br />"));
+            }
+        } catch (Exception e) {
         }
-        
+
         return "comments.xhtml?faces-redirect=true";
     }
-    
+
     
     public String sendComment() {
-        
+
         this.setCommentObject(new Comment());
         this.getCommentObject().setComment(this.getComment());
-        
+
         // lier le commentaire à la tâche.
         this.commentObject.setTaskId(new Task(project.getId()));
-        
-        
+
+
         commentHelper = new WSCommentHelper();
         commentHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
-        
+
         commentHelper.createNewComment(Comment.class, getCommentObject());
-        
+
         //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,"Sample warn message", "Watch out for PrimeFaces!"));
-        
+
         // refresh.
         this.showTaskComments();
         this.comment = "";
-        
+
         return "comments.xhtml?faces-redirect=true";
     }
 
@@ -188,5 +192,4 @@ public class CommentController {
     public void setCommentObject(Comment commentObject) {
         this.commentObject = commentObject;
     }
-    
 }
