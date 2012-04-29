b031e05dc80b04bb0db36a1a6fc758aa48f902b6
Michael Wermeester <michael.wermeester@me.com>
2012-04-29 21:24:04 +0200

    Make sending a bug report work.

---
+++ src/java/projetaBeans/ReportBugController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetaBeans;
+
+import be.luckycode.projetawebservice.Bug;
+import be.luckycode.projetawebservice.Bugcategory;
+import be.luckycode.projetawebservice.Project;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import pojeta.Common;
+import pojeta.ProjectSimple;
+import pojeta.WSBugHelper;
+
+/**
+ *
+ * @author michael
+ */
+@ManagedBean
+@SessionScoped
+public class ReportBugController implements Serializable {
+    
+    private ProjectSimple project;
+            
+    private Bug bug;
+    
+    private WSBugHelper bugHelper;
+    
+    /**
+     * Creates a new instance of ReportBugController
+     */
+    public ReportBugController() {
+        
+        this.bug = new Bug();
+        this.bug.setBugcategoryId(new Bugcategory());
+    }
+
+    /**
+     * @return the bug
+     */
+    public Bug getBug() {
+        return bug;
+    }
+
+    /**
+     * @param bug the bug to set
+     */
+    public void setBug(Bug bug) {
+        this.bug = bug;
+    }
+
+    /**
+     * @return the project
+     */
+    public ProjectSimple getProject() {
+        return project;
+    }
+
+    /**
+     * @param project the project to set
+     */
+    public void setProject(ProjectSimple project) {
+        this.project = project;
+    }
+
+    public String showBugReportForm() {
+        
+        this.bug.setProjectId(new Project(project.getId()));
+        
+        return "reportBug.xhtml?faces-redirect=true";
+    }
+    
+    public String sendBugReport() {
+        
+        bugHelper = new WSBugHelper();
+        bugHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+        
+        bugHelper.createNewBug(Bug.class, bug);
+        
+        return "bugReportConfirmation.xhtml?faces-redirect=true";
+    }
+}
