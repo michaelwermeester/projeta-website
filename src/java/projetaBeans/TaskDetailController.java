f823f44ded1e2532b854455f5de84b5a6cd48c67
Michael Wermeester <michael.wermeester@me.com>
2012-05-01 23:58:09 +0200

    Show username in task details.

--- src/java/projetaBeans/TaskDetailController.java
+++ src/java/projetaBeans/TaskDetailController.java
@@ -6,13 +6,11 @@ package projetaBeans;
 
 import be.luckycode.projetawebservice.Project;
 import be.luckycode.projetawebservice.Task;
+import be.luckycode.projetawebservice.User;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
-import pojeta.Common;
-import pojeta.ProjectSimple;
-import pojeta.WSProjectHelper;
-import pojeta.WSTaskHelper;
+import pojeta.*;
 
 /**
  *
@@ -27,39 +25,36 @@ public class TaskDetailController {
      */
     //@ManagedProperty("#{param.strProjectId}")
     private String strProjectId;
-    
     private Integer taskId;
-    
     private ProjectSimple projectSimple;
-    
     private Task task;
-    
     private WSTaskHelper taskHelper;
-    
+    private WSUserHelper userHelper;
+
     public TaskDetailController() {
     }
-    
+
     public String showTaskId(int id) {
         //this.projectId = Integer.parseInt(id);
         //this.projectId = Integer.parseInt(strProjectId);
         this.setTaskId((Integer) id);
-        
+
         return "taskDetail.xhtml";
     }
-    
+
     public String showTaskDetails() {
-        
+
         //this.projectId = Integer.parseInt(getStrProjectId());
-        
+
         this.setTaskId((Integer) projectSimple.getId());
-        
+
         taskHelper = new WSTaskHelper();
         taskHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
-        
+
         this.task = taskHelper.find(Task.class, this.taskId.toString());
-        
+
         return "taskDetail.xhtml?faces-redirect=true";
-     
+
     }
 
     /**
@@ -117,6 +112,22 @@ public class TaskDetailController {
     public void setTaskId(Integer taskId) {
         this.taskId = taskId;
     }
-    
-    
+
+    // retourne le nom de l'utilisateur qui a créé la tâche.
+    public String getUserCreated() {
+
+        try {
+            // instancier l'objet web-service et s'authentifier.
+            userHelper = new WSUserHelper();
+            userHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+
+            // obtenir l'objet utilisateur.
+            User user = userHelper.find(User.class, this.task.getUserCreated().getUserId().toString());
+
+            // retourner son nom.
+            return user.getUsername() + " " + user.getFullName();
+        } catch (Exception e) {
+            return "";
+        }
+    }
 }
