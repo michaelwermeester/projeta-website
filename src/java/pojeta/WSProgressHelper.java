34d45420d30db5bac2e0db90280c26994bcf637a
Michael Wermeester <michael.wermeester@me.com>
2012-05-02 23:27:42 +0200

    Add ProgressCocoaController.

---
+++ src/java/pojeta/WSProgressHelper.java
@@ -0,0 +1,146 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojeta;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+
+/**
+ * Jersey REST client generated for REST resource:ProgressFacadeREST
+ * [progress]<br>
+ *  USAGE:
+ * <pre>
+ *        WSProgressHelper client = new WSProgressHelper();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author michael
+ */
+public class WSProgressHelper {
+    private WebResource webResource;
+    private Client client;
+    private static final String BASE_URI = "https://luckycode.be:8181/projeta-webservice/resources";
+
+    public WSProgressHelper() {
+        com.sun.jersey.api.client.config.ClientConfig config = new com.sun.jersey.api.client.config.DefaultClientConfig(); // SSL configuration
+        config.getProperties().put(com.sun.jersey.client.urlconnection.HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new com.sun.jersey.client.urlconnection.HTTPSProperties(getHostnameVerifier(), getSSLContext()));
+        client = Client.create(config);
+        webResource = client.resource(BASE_URI).path("progress");
+    }
+
+    public <T> T findProgressByProjectIdWebsite(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("wsproject/{0}", new Object[]{id}));
+        return resource.get(responseType);
+    }
+
+    public void remove(String id) throws UniformInterfaceException {
+        webResource.path(java.text.MessageFormat.format("{0}", new Object[]{id})).delete();
+    }
+
+    public String countREST() throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path("count");
+        return resource.accept(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+    }
+
+    public <T> T findAll(Class<T> responseType) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public void edit_XML(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_XML).put(requestEntity);
+    }
+
+    public void edit_JSON(Object requestEntity) throws UniformInterfaceException {
+        webResource.type(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(requestEntity);
+    }
+
+    public <T> T findProgressByTaskIdWebsite(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("wstask/{0}", new Object[]{id}));
+        return resource.get(responseType);
+    }
+
+    public <T> T findRange_XML(Class<T> responseType, String from, String to) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{from, to}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T findRange_JSON(Class<T> responseType, String from, String to) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{from, to}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public <T> T find_XML(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T find_JSON(Class<T> responseType, String id) throws UniformInterfaceException {
+        WebResource resource = webResource;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
+        return resource.accept(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(responseType);
+    }
+
+    public <T> T createNewProgress(Class<T> responseType, Object requestEntity) throws UniformInterfaceException {
+        return webResource.path("create").type(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(responseType, requestEntity);
+    }
+
+    public void close() {
+        client.destroy();
+    }
+
+    public void setUsernamePassword(String username, String password) {
+        client.addFilter(new com.sun.jersey.api.client.filter.HTTPBasicAuthFilter(username, password));
+    }
+
+    private HostnameVerifier getHostnameVerifier() {
+        return new HostnameVerifier() {
+
+            @Override
+            public boolean verify(String hostname, javax.net.ssl.SSLSession sslSession) {
+                return true;
+            }
+        };
+    }
+
+    private SSLContext getSSLContext() {
+        javax.net.ssl.TrustManager x509 = new javax.net.ssl.X509TrustManager() {
+
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] arg0, String arg1) throws java.security.cert.CertificateException {
+                return;
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] arg0, String arg1) throws java.security.cert.CertificateException {
+                return;
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+        };
+        SSLContext ctx = null;
+        try {
+            ctx = SSLContext.getInstance("SSL");
+            ctx.init(null, new javax.net.ssl.TrustManager[]{x509}, null);
+        } catch (java.security.GeneralSecurityException ex) {
+        }
+        return ctx;
+    }
+    
+}
---
+++ src/java/projetaBeans/ProgressCocoaController.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetaBeans;
+
+import be.luckycode.projetawebservice.Comment;
+import be.luckycode.projetawebservice.CommentDummy;
+import be.luckycode.projetawebservice.Progress;
+import be.luckycode.projetawebservice.ProgressDummy;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import pojeta.Common;
+import pojeta.WSCommentHelper;
+import pojeta.WSProgressHelper;
+
+/**
+ *
+ * @author michael
+ */
+@ManagedBean
+@RequestScoped
+public class ProgressCocoaController {
+
+    private List<Progress> progressList;
+    private WSProgressHelper progressHelper;
+    // ID de du projet, tâche, bug.
+    private String requestId;
+    // type (project, task, bug).
+    private String requestType;
+    
+    /**
+     * Creates a new instance of ProgressCocoaController
+     */
+    public ProgressCocoaController() {
+        
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        // lire ID de du projet, tâche, bug.
+        this.requestId = request.getParameter("id");
+        // lire le type (projet, tâche, bug).
+        this.requestType = request.getParameter("type");
+    }
+
+    /**
+     * @return the progressList
+     */
+    public List<Progress> getProgressList() {
+        
+        try {
+            progressHelper = new WSProgressHelper();
+            progressHelper.setUsernamePassword(Common.getWSUsername(), Common.getWSPassword());
+
+            // si le type = "task".
+            if (requestType.equals("task")) {
+                ProgressDummy tmpProgressDummy = progressHelper.findProgressByTaskIdWebsite(ProgressDummy.class, this.requestId);
+
+                this.progressList = tmpProgressDummy.getListProgress();
+            } // si le type = "project".
+            else if (requestType.equals("project")) {
+                ProgressDummy tmpProgressDummy = progressHelper.findProgressByProjectIdWebsite(ProgressDummy.class, this.requestId);
+
+                this.progressList = tmpProgressDummy.getListProgress();
+            }
+
+            // line breaks.
+            for (Progress p : progressList) {
+                p.setProgressComment(p.getProgressComment().replace("\n", "<br />"));
+            }
+        } catch (Exception e) {
+        }
+
+        return progressList;
+    }
+
+    /**
+     * @param progressList the progressList to set
+     */
+    public void setProgressList(List<Progress> progressList) {
+        this.progressList = progressList;
+    }
+}
